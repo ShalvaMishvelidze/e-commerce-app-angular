dbab8ca6f4f313805eac24b232e013de1875fac7
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-05-26 22:11:26 +0400

    implemented rudimentary add to cart functionality on click of the product

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/cart/cart.service';
 
 @Component({
   selector: 'app-product-list',
@@ -9,10 +10,18 @@ import { Product } from 'src/app/models/product';
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private cartService: CartService
+  ) {}
   ngOnInit(): void {
     this.productService.getProducts().subscribe((data) => {
       this.products = data;
+    });
+  }
+  addToCart(product: Product): void {
+    this.cartService.addToCart(product).subscribe((response) => {
+      alert(`${product.name} has been added to your cart!`);
     });
   }
 }
